6adfcd7f44d97da2c1a0c9be41b7c69a50f293ae
Madhanbabus293 <46224185+Madhanbabus293@users.noreply.github.com>
2019-05-29 16:16:40 +0530

    Add files via upload

--- subArrayZero.java
+++ subArrayZero.java
@@ -1,28 +1,31 @@
 import java.util.*;
 public class subArrayZero {
-public static void main(String[] args) {
-int sum=0,flag=0,count=0;
-Scanner s=new Scanner(System.in);
-int l=s.nextInt();
-int[] arr=new int[l];
-for(int i=0;i<arr.length;i++) {
-arr[i]=s.nextInt();
-}
-for(int i=0;i<arr.length;i++) {
-for(int j=i;j<arr.length;j++) {
-sum=sum+arr[j];
-if(sum==0) {
-count+=1;
-System.out.print("True");
-break;
-}
-else {
-flag=1;
-}
-}
-sum=0;
-}
-if(flag!=0&&count==0) {
-System.out.print("false");
-}
-}}
+  public static void main(String args[]) {
+    Scanner s=new Scanner(System.in);
+    int l=s.nextInt();
+    int[] arr=new int[l];
+    for(int i=0;i<l;i++) {
+    arr[i]=s.nextInt();
+    }
+    //int l=5;
+    //int[] arr={4,2,-3,1,6};
+    int flag=0,sum=0;
+    for(int i=0;i<l;i++) {
+sum=sum+arr[i];
+      for(int j=i+1;j<l;j++) {
+              sum=sum+arr[j];
+              if(sum==0) {
+                flag=1;
+                break;
+              }
+      }
+      sum=0;
+    }
+      if(flag==1) {
+        System.out.println("True");
+      }
+      else 
+      {
+        System.out.println("False");
+      }
+  }}
